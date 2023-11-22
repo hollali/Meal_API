3c0b87823f28cb562d36cfd3ef8d9ef2b7b330d4
hollali <dheztinykartel@gmail.com>
2023-11-22 00:21:32 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,51 @@
+# Meal Search Web App
+
+## Overview
+
+This is a simple web application that allows users to search for meals and discover random recipes. It utilizes HTML for structuring the content, CSS for styling, and JavaScript for interactivity. The app fetches meal data from an external API and displays the results dynamically on the page.
+
+## Features
+
+- **Search for Meal**: Users can enter a meal name in the search bar to find specific recipes.
+- **Random Meal**: Users can click the "Random" button to discover a random meal recipe.
+- **Dynamic Display**: Meal search results are dynamically displayed on the page.
+
+## Technologies Used
+
+- **HTML**: For structuring the content of the web page.
+- **CSS**: For styling and layout design.
+- **JavaScript**: For fetching data from an API and dynamically updating the page.
+- **Font Awesome**: For icons in the search button.
+
+## Project Structure
+
+- **index.html**: The main HTML file containing the structure of the web page.
+- **css/index.css**: CSS file for styling the web page.
+- **script/index.js**: JavaScript file for fetching data and updating the page dynamically.
+
+## Usage
+
+1. Open the `index.html` file in a web browser.
+
+2. Enter a meal name in the search bar and click the search button to find specific recipes.
+
+3. Click the "Random" button to discover a random meal recipe.
+
+4. Explore the dynamically displayed meal search results.
+
+5. Click on a specific meal to view detailed information, including ingredients and instructions.
+
+## Additional Notes
+
+- This project uses the [MealDB API](https://www.themealdb.com/api.php) to fetch meal data.
+- Ensure that your internet connection is active to fetch data from the API.
+
+## Credits
+
+This project was created by Hollali Kelvin.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+Feel free to contribute or report issues!
---
+++ script/index.js
@@ -0,0 +1,106 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const resultHeading = document.getElementById('meal-result-heading');
+const mealsEl = document.getElementById('meals');
+const single_mealEl = document.getElementById("single-meal-container");
+
+function findMeal(e) {
+    e.preventDefault();
+    const item = search.value
+    if(item.trim()){
+        //fetch api data display in browser//
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`)
+        .then((res) => res.json())
+        .then((data)=>{
+            resultHeading.innerHTML = `Search result for ${item}`;
+            //if condition
+        if (data.meals === null) {
+            resultHeading.innerHTML = `Oops !! No  result for ${item}`;
+        }else{
+            mealsEl.innerHTML = data.meals.map((meal) => `
+            <div class="meal">
+            <img src ="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="meal-info" data-mealId="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+            </div>
+            </div>`)
+            .join("");
+        }
+        }); 
+        // clear input field
+        search.value = "";
+    }else{
+        alert("Please input Data");
+    }
+}
+// function to get meal id
+function getsingleItemID(mealId){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res)=> res.json())
+    .then((data)=>{
+        console.log(data);
+        const meal = data.meals[0];
+        addMealToDOM(meal);
+    });
+}
+// function to get random meals
+function getRandomMeal() {
+    // clear result data and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) =>res.json())
+    .then((data) => {
+        const meal = data.meals[0];
+        addMealToDOM(meal);
+    });
+}
+//function to add meals to dom
+function addMealToDOM(meal) {
+    const ingredients = [];
+    for (let i=1; i<=20; i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        }else{
+            break;
+        }
+    }
+    single_mealEl.innerHTML = `
+    <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <div class="single-meal-info">
+        ${meal.strCategory?`<p>${meal.strCategory}</p>`:""}
+        ${meal.strArea?`<p>${meal.strArea}</p>`:""}
+    </div>
+    <img src="${meal.strMealThumb}" alt="${meal.strArea}">
+    <div class="main">
+        <h2>Ingredients</h2>
+            <ul>
+                ${ingredients.map((ing)=>
+                    `<li>${ing}</li>`
+                ).join("")}
+            </ul>
+            <h2>Instructions</h2>
+            <p>${meal.strInstructions}</p>
+    </div>
+    </div>`;
+} 
+submit.addEventListener("submit", findMeal);
+random.addEventListener('click', getRandomMeal)
+
+//single meal click
+mealsEl.addEventListener("click", (e) => {
+    const mealInfo = e.composedPath().find((single_item) => 
+    {
+        if(single_item.classList){
+            return single_item.classList.contains('meal-info');
+        }else{
+            return false;
+        }
+    });
+    if (mealInfo){
+        const mealId = mealInfo.getAttribute("data-mealId");
+        getsingleItemID(mealId);
+    }
+});
